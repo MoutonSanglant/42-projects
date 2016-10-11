07e9382a40e52c74b92f8ae834a2246719ad9552
MoutonSanglant <defresne.thierry@gmail.com>
2016-10-11 15:47:37 +0200

    ->

--- libft/src/ft_debug_error_free.c
+++ libft/src/ft_debug_error_free.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/05 20:19:56 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/03/05 20:21:30 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/10/11 10:25:10 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void			ft_debug_error_free(const char *fn_name)
 {
-	ft_printf("\n\x1B[31mERROR in libft.a: the function %s %s\n", fn_name,
-			"returned the following error:\ncandidate for free is NULL. \
+	ft_putstr("\n\x1B[31mERROR in libft.a: the function ");
+	ft_putstr(fn_name);
+	ft_putendl("returned the following error:\ncandidate for free is NULL. \
 			Verify your code, it is unsafe !!!\x1B[0m");
 }
--- libft/src/ft_debug_error_param.c
+++ libft/src/ft_debug_error_param.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/02 18:34:08 by tdefresn          #+#    #+#             */
-/*   Updated: 2016/03/02 18:38:25 by tdefresn         ###   ########.fr       */
+/*   Updated: 2016/10/11 10:25:03 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void			ft_debug_error_param(const char *fn_name)
 {
-	ft_printf("\n\x1B[31mERROR in libft.a: the function %s %s\n", fn_name,
-			"returned the following error:\nSent parameter is NULL. \
+	ft_putstr("\n\x1B[31mERROR in libft.a: the function ");
+	ft_putstr(fn_name);
+	ft_putendl("returned the following error:\nSent parameter is NULL. \
 			Verify your code, it is unsafe !!!\x1B[0m");
 }
