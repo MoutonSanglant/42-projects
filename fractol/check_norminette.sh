fd78d3ecfd9ec5b8aa4b90feebcd8288d1cf56b0
MoutonSanglant <defresne.thierry@gmail.com>
2016-11-10 01:11:13 +0100

    removed comments

--- fractol/check_norminette.sh
+++ fractol/check_norminette.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-##/Users/tdefresn/.brew/Cellar/bash/4.4_1/bin/bash
-
-#echo "This script requires at least bash 4.0"
-
-
-#shopt -s extglob
-
-#ls (*/)5/*[!mlx][!mlx_int][!mlx_new_window].[ch]
-#ls ***/!(*mlx).[ch]
-
 find -L . -type f \( -iname "*.[ch]" -a -not -iname "*mlx*" \) -exec norminette {} +
-#	echo "a"
-#	norminette "$line"
-#done
