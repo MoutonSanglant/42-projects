8df88999588663008a82da4759188c3b6023075a
MoutonSanglant <defresne.thierry@gmail.com>
2017-09-17 00:38:19 +0200

    Return vectors to chained calls

--- common/src/math/vector.c
+++ common/src/math/vector.c
@@ -6,35 +6,43 @@
 /*   By: mouton </var/spool/mail/mouton>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/12 22:38:33 by mouton            #+#    #+#             */
-/*   Updated: 2017/09/12 22:51:15 by mouton           ###   ########.fr       */
+/*   Updated: 2017/09/13 22:11:02 by mouton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 
-void vec2(t_vec2 *vector, int x, int y)
+t_vec2 *vec2(t_vec2 *vector, int x, int y)
 {
 	vector->x = x;
 	vector->y = y;
+
+	return vector;
 }
 
-void vec3(t_vec3 *vector, int x, int y, int z)
+t_vec3 *vec3(t_vec3 *vector, int x, int y, int z)
 {
 	vector->x = x;
 	vector->y = y;
 	vector->z = z;
+
+	return vector;
 }
 
-void vec2f(t_vec2f *vector, float x, float y)
+t_vec2f *vec2f(t_vec2f *vector, float x, float y)
 {
 	vector->x = x;
 	vector->y = y;
+
+	return vector;
 }
 
-void vec3f(t_vec3f *vector, float x, float y, float z)
+t_vec3f *vec3f(t_vec3f *vector, float x, float y, float z)
 {
 	vector->x = x;
 	vector->y = y;
 	vector->z = z;
+
+	return vector;
 }
 
